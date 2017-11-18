353dbc693cb1140ee1e610a22143f7896ae87722
Henrik Dahlberg <hdahlb@kth.se>
2017-11-18 15:29:43 +0100

    Add VS Code settings, keybindings and install script for extensions

--- symlinks.bat
+++ symlinks.bat
@@ -1,1 +1,4 @@
 mklink %HOMEPATH%\.bashrc %HOMEPATH%\Documents\repos\scripts-etc\.bashrc
+
+mklink %APPDATA%\Code\User\settings.json %HOMEPATH%\Documents\repos\scripts-etc\vscode\settings.json
+mklink %APPDATA%\Code\User\keybindings.json %HOMEPATH%\Documents\repos\scripts-etc\vscode\keybindings.json
