ec6c15729e2cdb363c81b4c8f78e54e7402b3c72
Henrik Dahlberg <hda@vpd.se>
2017-12-11 11:52:56 +0100

    add Hyper Terminal settings

--- symlinks.bat
+++ symlinks.bat
@@ -1,4 +1,5 @@
 mklink %USERPROFILE%\.bashrc %USERPROFILE%\Documents\repos\scripts-etc\.bashrc
+mklink %USERPROFILE%\.hyper.js %USERPROFILE%\Documents\repos\scripts-etc\.hyper.js
 
 mklink %APPDATA%\Code\User\settings.json %USERPROFILE%\Documents\repos\scripts-etc\vscode\settings.json
 mklink %APPDATA%\Code\User\keybindings.json %USERPROFILE%\Documents\repos\scripts-etc\vscode\keybindings.json
