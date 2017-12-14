43f4fa8a61b600dede3557e6a9ef9ef741e94eec
Henrik Dahlberg <hda@vpd.se>
2017-12-14 10:29:18 +0100

    add firefox theme symlink

--- symlinks.bat
+++ symlinks.bat
@@ -3,3 +3,5 @@ mklink %USERPROFILE%\.hyper.js %USERPROFILE%\Documents\repos\scripts-etc\.hyper.
 
 mklink %APPDATA%\Code\User\settings.json %USERPROFILE%\Documents\repos\scripts-etc\vscode\settings.json
 mklink %APPDATA%\Code\User\keybindings.json %USERPROFILE%\Documents\repos\scripts-etc\vscode\keybindings.json
+
+mklink %APPDATA%\Mozilla\Firefox\Profiles\7t97ofsg.default\chrome\userChrome.css %USERPROFILE%\Documents\repos\scripts-etc\firefox\userChrome.css
